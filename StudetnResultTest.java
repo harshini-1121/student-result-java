dbeb15ccbd5bddfecdfb70b7553651b4c04c1f7a
harshini-1121 <71762333015@cit.edu.in>
2026-02-14 11:50:56 +0530

    Add StudentResult class to evaluate student results

---
+++ StudetnResultTest.java
@@ -0,0 +1,19 @@
+public class StudentResult {
+
+    public static String getResult(int marks) {
+
+        if(marks >= 40){
+            return "PASS";
+        } else {
+            return "FAIL";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        System.out.println("Student Marks: " + marks);
+        System.out.println("Result: " + getResult(marks));
+    }
+}
